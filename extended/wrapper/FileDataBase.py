3acacf16217fbfa4cf25889598f73d6d571506a4
mingotang <mtang024@126.com>
2020-11-24 08:57:12 +0800

    Update FileDataBase.py

--- extended/wrapper/FileDataBase.py
+++ extended/wrapper/FileDataBase.py
@@ -1,6 +1,10 @@
 # -*- encoding: UTF-8 -*-
 # ---------------------------------import------------------------------------
+import json
 import os
+
+
+FILE_ENCODING = 'UTF-8'
 
 
 class FileDataBase(object):
@@ -10,11 +14,13 @@ class FileDataBase(object):
         self.__path__ = db_path
 
     def show_data_bases(self):
+        """搜索文档目录，返回寻找到的数据集合"""
         # 搜寻文档目录
         db_list = list()
         for item_name in os.listdir(self.__path__):
+            # 判断是否是Sector并返回Sector
             if os.path.isdir(os.path.join(self.__path__, item_name)) and os.path.exists(
-                os.path.join(self.__path__, item_name, '__db_index__.json')
+                os.path.join(self.__path__, item_name, BaseSector.__index_file_name__)
             ):
                 db_list.append(os.path.join(self.__path__, item_name))
             else:
@@ -23,6 +29,16 @@ class FileDataBase(object):
 
 
 class BaseSector(object):
+    """数据集合，内含若干表格"""
+    __index_file_name__ = '__sector_index__.json'
 
     def __init__(self, sector_path: str):
         self.__path__ = sector_path
+        self.structure = json.load(open(os.path.join(self.__path__, )))
+
+    def __repr__(self):
+        return 'FileBaseSector {}, Located {}'.format(self.name, self.__path__)
+
+    @property
+    def name(self):
+        return self.__path__.split(os.path.sep)[-1]
